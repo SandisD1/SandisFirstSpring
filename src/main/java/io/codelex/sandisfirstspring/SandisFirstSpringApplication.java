6850a5bcca3c8a62ff9ae079d4c64e92d6d9c5f4
SandisD1 <sandisdreimanis2@gmail.com>
2022-10-21 20:11:45 +0300

    Creating Repo

---
+++ src/main/java/io/codelex/sandisfirstspring/SandisFirstSpringApplication.java
@@ -0,0 +1,13 @@
+package io.codelex.sandisfirstspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandisFirstSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandisFirstSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/codelex/sandisfirstspring/SandisFirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codelex.sandisfirstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandisFirstSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
